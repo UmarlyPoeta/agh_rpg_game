51e6c6a0c8a5f985365cc6f4e04f0872124e95c6
UmarlyPoeta <pkozlowski@student.agh.edu.pl>
2024-12-05 02:39:29 +0100

    Rename project from StudentRPG to AghRPG and update window title in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 enum GameState { MENU, MAP, BATTLE };
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Student RPG");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "AGH RPG");
 
     GameState state = MENU;
 

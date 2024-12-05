3d812d1f799bc87d443c3a7bb4765f2cc701eaa1
UmarlyPoeta <pkozlowski@student.agh.edu.pl>
2024-12-05 02:11:10 +0100

    Add main game loop and player movement with SFML; remove old main.cpp and configuration files

---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+#include <SFML/Graphics.hpp>
+#include "Menu.hpp"
+#include "Map.hpp"
+#include "Battle.hpp"
+
+enum GameState { MENU, MAP, BATTLE };
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Student RPG");
+
+    GameState state = MENU;
+
+    Menu menu(window);
+    Map map(window);
+    Battle battle(window);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) 
+                window.close();
+        }
+
+        window.clear();
+
+        switch (state) {
+            case MENU:
+                if (menu.run()) state = MAP;
+                break;
+            case MAP:
+                if (map.run()) state = BATTLE;
+                break;
+            case BATTLE:
+                if (battle.run()) state = MAP;
+                break;
+        }
+
+        window.display();
+    }
+
+    return 0;
+}
